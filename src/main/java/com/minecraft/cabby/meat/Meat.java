308835a3dbd2dbaca71cd074058662c724dd7cb2
7bitcatt <77202713+7bitcatt@users.noreply.github.com>
2021-12-18 15:44:45 -0500

    more init

---
+++ src/main/java/com/minecraft/cabby/meat/Meat.java
@@ -0,0 +1,10 @@
+package com.minecraft.cabby.meat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Meat implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/minecraft/cabby/meat/client/MeatClient.java
@@ -0,0 +1,13 @@
+package com.minecraft.cabby.meat.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MeatClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
